bd04c3d142a48b275587db43507995447fd18a19
kg4543 <jyc351919@naver.com>
2021-02-22 17:27:36 +0900

    4장 시작

---
+++ chap04/Chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26; // 26
+            Console.WriteLine($"현재 숫자는 {result}");
+            result += 3; // 29
+            Console.WriteLine($"현재 숫자는 {result}");
+            result -= 10; // 19
+            Console.WriteLine($"현재 숫자는 {result}"); 
+            Console.WriteLine($"현재 숫자는 {result++}"); // 19 출력 후 20으로 증가
+            Console.WriteLine($"현재 숫자는 {++result}"); // 21로 증가 후 출력
+            Console.WriteLine($"현재 숫자는 {result--}"); // 21 출력 후 20으로 감소
+            Console.WriteLine($"현재 숫자는 {--result}"); // 19로 감소 후 출력
+            result += 1; //20
+            Console.WriteLine($"현재 숫자는 {result}");
+
+        }
+    }
+}
