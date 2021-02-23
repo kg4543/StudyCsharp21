65faca1742cdeb3cd665d0c77428efd919e8fba6
kg4543 <jyc351919@naver.com>
2021-02-23 17:22:52 +0900

    20210223_17:22

---
+++ chap06/Chap06App/ProfileTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintProfile("김구겸", "010-4521-8100");
+
+            PrintProfile(null, "010-4568-8745");
+        }
+
+        public static void PrintProfile(string name,string phone)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("이름을 정확히 입력하세요");
+                return;
+            }
+            //프로필 출력
+            Console.WriteLine($"이름 : {name}, 폰번호 {phone}");
+        }
+    }
+}
