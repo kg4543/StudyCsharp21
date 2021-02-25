f4b8f73a9bc6466a072266a17c36ab117071efde
kg4543 <jyc351919@naver.com>
2021-02-25 12:31:03 +0900

    7장 완료

---
+++ chap07/Chap07App/PartialClassApp/Myclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartialClassApp
+{
+    partial class Myclass
+    {
+        public void Method1() { Console.WriteLine("Method1"); }
+        public void Method2() { Console.WriteLine("Method2"); }
+
+    }
+}
