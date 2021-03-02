8bf647690fb479ce13c0f0a9712a0d7a1451cdf0
kg4543 <jyc351919@naver.com>
2021-03-02 15:30:51 +0900

    12장 완료

---
+++ chap12/Chap12App/Chap12App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap12App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            for (int i = 0; i < 5; i++)  // '< 5'는 실행 시 오류 
+            {
+                Console.WriteLine($"{i} 번째 값 : {arr[i]}");
+            }
+        }
+    }
+}
