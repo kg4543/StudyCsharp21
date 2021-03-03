b1f019927421fc0a5f0335db5953b05ec1e27bd5
kg4543 <jyc351919@naver.com>
2021-03-03 17:30:00 +0900

    20210303

---
+++ chap18/Chap18App/GenDirFireApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace GenDirFireApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strDir = "D:\\GitRepository\\StudyCsharp21\\SampleDir"; //폴더,파일 생성 테스트 폴더
+            string strTargetDir = "SubFolder"; //생성하고자하는 폴더명
+            string strTagetFile = "readme.txt";
+
+            string targetPath = strDir + "\\" + strTargetDir;
+            //string targetPath = $(strDir\\strTargetDir);
+
+            if (Directory.Exists(targetPath))
+            {
+                Console.WriteLine("이미 폴더가 존재합니다.");
+            }
+            else
+            {
+                Directory.CreateDirectory(targetPath);
+                Console.WriteLine("폴더 생성 성공!!");
+            }
+
+            targetPath += $"\\{strTagetFile}";
+            if (File.Exists(targetPath))
+            {
+                Console.WriteLine("이미 파일이 존재합니다.");
+            }
+            else
+            {
+                File.Create(targetPath).Close();
+                Console.WriteLine("파일 생성 성공!!");
+            }
+        }
+    }
+}
---
+++ chap18/Chap18App/TextFileApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace TextFileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\GitRepository\StudyCsharp21\SampleDir\SubFolder\a.dat"; //텍스트 파일 생성위치
+
+            StreamWriter sw = null; 
+
+            try
+            {
+                sw = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate,FileAccess.Write));
+                sw.WriteLine($"int.MaxValue = {int.MaxValue}");
+                sw.WriteLine("Hello, World!");
+                sw.WriteLine($"unit.MaxValue = {uint.MaxValue}");
+                sw.WriteLine("안녕하세요!!");
+                sw.WriteLine($"unit.MaxValue = {double.MaxValue}");
+
+                Console.WriteLine("파일 생성");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"파일 쓰기 예외발생 : {ex.Message}");
+            }
+            finally
+            {
+                sw.Close();
+            }
+
+            StreamReader sr = null;
+            sr = new StreamReader(new FileStream(filePath, FileMode.Open,FileAccess.Read));
+            Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+
+            while (sr.EndOfStream == false)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+        }
+    }
+}
